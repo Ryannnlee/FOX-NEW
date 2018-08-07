7eeba89cfc4659de2a8df69a8f33f06f7e40223b
Ryan L <lee810317@hotmail.com.tw>
2018-08-07 17:22:20 +0800

    20180807  fix aboutus & article menu

--- build/assets/js/aboutus.js
+++ build/assets/js/aboutus.js
@@ -1,6 +1,17 @@
-"use strict";
+'use strict';
 
 $(function () {
-    // var WH = $(window).height();
-    // $('#section0').height(WH);
+    var h = $(window).height();
+    if ($(window).scrollTop() < h / 5) {
+        $('header').addClass("menuScroll");
+    } else {
+        $('header').removeClass("menuScroll");
+    }
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > h / 5) {
+            $('header').removeClass("menuScroll");
+        } else {
+            $('header').addClass("menuScroll");
+        }
+    });
 });
--- src/assets/js/article.js
+++ src/assets/js/article.js
@@ -1,4 +1,18 @@
 $(function() {
     var WH = $(window).height();
     $('#section0').height(WH);
+
+    var h = $(window).height();
+    if ($(window).scrollTop() < h / 5) {
+        $('header').addClass("menuScroll");
+    } else {
+        $('header').removeClass("menuScroll");
+    }
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() > h / 5) {
+            $('header').removeClass("menuScroll");
+        } else {
+            $('header').addClass("menuScroll");
+        }
+    });
 });
